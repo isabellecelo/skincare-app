6d6c6dbef3d6e143881aeda85485eb7cfb2d3f61
isabellecelo <isabelle.celo04@gmail.com>
2024-09-14 11:26:38 -0700

    Initial Commit

---
+++ skincare-app/skincare_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  skincare_appApp.swift
+//  skincare-app
+//
+//  Created by Isabelle Celo on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct skincare_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
